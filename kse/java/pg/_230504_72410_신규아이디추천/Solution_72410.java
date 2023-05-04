8f15bc638d079e79bea40c6e06e393ca71c1be82
김승은 <tmddms0317@naver.com>
2023-05-04 13:24:47 +0900

    fix : 신규 아이디 추천 - 정규표현식 사용

--- kse/java/pg/_230504_72410_신규아이디추천/Solution_72410.java
+++ kse/java/pg/_230504_72410_신규아이디추천/Solution_72410.java
@@ -1,21 +1,17 @@
 package java.pg._230504_72410_신규아이디추천;
-
+//https://school.programmers.co.kr/learn/courses/30/lessons/72410
 public class Solution_72410 {
     public String solution(String new_id) {
         String answer = "";
+
         //1_대문자를 소문자로 치환
         new_id = new_id.toLowerCase();
 
         //2_알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거
-        String temp = "";
-        for(char s : new_id.toCharArray()){
-            if('a'<=s && s <= 'z') temp += s;
-            else if('0'<=s && s <= '9') temp += s;
-            else if(s=='-' || s=='_' || s=='.') temp += s;
-        } new_id = temp;
+        new_id = new_id.replaceAll("[^a-z0-9._-]","");
 
         //3_마침표 정리
-        temp = "";
+        String temp = "";
         boolean point = false;
         for(char s : new_id.toCharArray()){
             if(s == '.'){
@@ -28,8 +24,11 @@ public class Solution_72410 {
                 point = false;
             }
         }
+
         //4_처음 끝 제거
-        if(temp.charAt(0)=='.')temp = temp.substring(1);
+        if(!temp.isEmpty()){
+            if(temp.charAt(0)=='.')temp = temp.substring(1);
+        }
         if(!temp.isEmpty()){
             if(temp.charAt(temp.length()-1)=='.')
                 temp = temp.substring(0, temp.length()-1);}
