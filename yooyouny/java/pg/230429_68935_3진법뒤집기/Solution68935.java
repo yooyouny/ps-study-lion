59d2d82277d7ab5c4c070a074e7a309f513fde65
김승은 <71924061+kse0312@users.noreply.github.com>
2023-05-22 20:13:20 +0900

    Delete yooyouny/java/pg directory

--- yooyouny/java/pg/230429_68935_3진법뒤집기/Solution68935.java
+++
@@ -1,6 +0,0 @@
-class Solution68935 {
-	public int solution(int n) {
-		StringBuilder sb = new StringBuilder(Integer.toString(n, 3));// reverse를 쓰기 위해 stringBuilder 사용. 3진법 String으로 바꾸고
-		return Integer.parseInt(sb.reverse().toString(), 3);// 3진법 String을 10진수로 바꾸기
-	}
-}
