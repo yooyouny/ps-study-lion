738df550b93a1db02faac2220804b1f0d60ba4cc
harper <jhjin2u@gmail.com>
2023-06-16 21:50:39 +0900

    신규_백준_bronze_Math

---
+++ yooyouny/java/boj/Math/Main2501.java
@@ -0,0 +1,28 @@
+package java.boj.Math;
+
+import java.util.Scanner;
+
+public class Main2501 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		int[] answer = new int[100000];
+		int num = 1, idx = 0;
+		while(N >= num){
+			if(N % num == 0) {
+				answer[idx++] = num;
+			}
+			if (idx == k) {
+				System.out.println(num);
+				break;
+			}
+			num++;
+		}
+		if (idx != k)
+			System.out.println(0);
+
+	}
+
+}
