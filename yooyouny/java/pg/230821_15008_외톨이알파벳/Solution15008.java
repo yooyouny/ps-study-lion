e7c2efc8c2776b7ee6842ec8c25f500931a61e8b
yooyouny <jhjin2u@hanmail.com>
2023-08-21 15:26:36 +0900

    신규_프로그래머스_lv1_15008_외톨이알파벳 - Set활용 문자열처리

---
+++ yooyouny/java/pg/230821_15008_외톨이알파벳/Solution15008.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution15008 {
+    public String solution(String input_string) {
+        Set<Character> visited = new HashSet<>();// 중복체크 set
+        Set<Character> alone = new TreeSet<>();// 외톨이숫자 set
+        char before = ' ';
+
+        for (char c : input_string.toCharArray()) {
+            if (before != c) {// 연속되지 않고
+                if (visited.contains(c)) {// 이전에 한번 등장했던 문자면 외톨이숫자
+                    alone.add(c);
+                }
+                visited.add(c);// 처음으로 등장한 알파벳은 set에 저장하여 방문처리
+                before = c;// 연속문자 체크를 위해 이전문자 저장
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();// Set to String
+        for (char c : alone) {
+            sb.append(c);
+        }
+
+        return sb.length() == 0 ? "N" : sb.toString();
+    }
+}
